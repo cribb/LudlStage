21cd79289fe710d54b2323e41219aacde4e75d37
Jeremy Cribb <cribb@cs.unc.edu>
2020-07-23 09:52:48 -0400

    Updated documentation.

--- stage_close.m
+++ stage_close.m
@@ -1,10 +1,8 @@
 function status = stage_close(S)
 % STAGE_CLOSE closes the connection to the Ludl stage
 
-    %--------------------------------------------------------------------------
-    % This function closes the serial port of the stage. Reference to the serial 
-    % port is passed to it as the object/structure 'S' with field 'handle.'
-    %--------------------------------------------------------------------------
+% This function closes the serial port of the stage. Reference to the serial 
+% port is passed to it as the object/structure 'S' with field 'handle.'
     
     fclose(S.handle);
     delete(S.handle);
