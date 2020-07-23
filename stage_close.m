8577951e0f6992f37551a28687d2e6ab2e7d806a
Jeremy Cribb <cribb@cs.unc.edu>
2020-07-23 09:28:32 -0400

    Changed input to work with the ludl object instead of the handle to the serial port for the ludl stage which contains the handle to the serial port.

--- stage_close.m
+++ stage_close.m
@@ -1,13 +1,13 @@
-function status=stage_close(S)
-
-% STAGE_CLOSE closes the connection to the stage
+function status = stage_close(S)
+% STAGE_CLOSE closes the connection to the Ludl stage
 
     %--------------------------------------------------------------------------
     % This function closes the serial port of the stage. Reference to the serial 
-    % port is passed to it as the argument S.
+    % port is passed to it as the object/structure 'S' with field 'handle.'
     %--------------------------------------------------------------------------
-    fclose(S);
-    delete(S);
+    
+    fclose(S.handle);
+    delete(S.handle);
     clear S;
     status=0;
 end
