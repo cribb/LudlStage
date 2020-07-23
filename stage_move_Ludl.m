6e43596dcbdf49a32c9bfc0bfbabe10a2d91947f
Jeremy Cribb <cribb@cs.unc.edu>
2020-07-23 09:55:17 -0400

    Cut out commented out code that opened a connection to the hardware if did not exist already and code that communicated with the user via a modal dialog box.

--- stage_move_Ludl.m
+++ stage_move_Ludl.m
@@ -16,24 +16,6 @@ function stage=stage_move_Ludl(stage, target_pos)
 % objects can be cleared from memory with
 % delete(objects)
 try
-    
-%     % If communications to stage is not open to start, then open.
-%     % Also prepare to close
-%     h_stage_close = 0;
-%     if exist('h_stage', 'var') == 0
-%         h_stage_close = 1;
-%         [stage] = stage_open_Ludl(stage.label);
-%         % If communications with the stage cannot be established,
-%         % eeDAP is closing.
-%         if stage.status == 0
-%             desc = ['Communications with the stage is not established.',...
-%                 'eeDAP is closing.'] %#ok<NOPRT>
-%             h_errordlg = errordlg(desc,'Application error','modal');
-%             uiwait(h_errordlg)
-%             close all force;
-%             return
-%         end
-%     end
 
     % Target position needs to be a 64 bit integer
     target_pos = int64(target_pos);
@@ -57,26 +39,23 @@ try
         ' y=',num2str(target_pos(2))];
     stage_send_com_Ludl(stage.handle, command_str);
     
-%     % Display the waitbar
-%     wtb=waitbar(0,'Moving the stage...', 'WindowStyle', 'modal');
-%     % Keep updating the waitbar
     while stage_check_busy_Ludl(stage.handle)
         stage=stage_get_pos_Ludl(stage,stage.handle) ;
         current_pos=stage.Pos;
         pause(.5)
-% %         distance_traveled = pdist2(...
-% %             double(Initial_Pos),...
-% %             double(current_pos),'euclidean');
-% %         distance_total = pdist2(...
-% %             double(Initial_Pos),...
-% %             double(target_pos),'euclidean');
-%         distance_traveled = sqrt((double(Initial_Pos(1))-double(current_pos(1)))^2 + (double(Initial_Pos(2))-double(current_pos(2)))^2);
-%         distance_total = sqrt((double(Initial_Pos(1))-double(target_pos(1)))^2 + (double(Initial_Pos(2))-double(target_pos(2)))^2);
+        
+        distance_traveled = pdist2( double(Initial_Pos), double(current_pos), 'euclidean');
+        distance_total    = pdist2( double(Initial_Pos), double(target_pos), 'euclidean');
+        
+%         distance_traveled = sqrt( (double(Initial_Pos(1)) - double(current_pos(1)))^2 + ...
+%                                   (double(Initial_Pos(2)) - double(current_pos(2)))^2);
+%         distance_total =    sqrt( (double(Initial_Pos(1)) - double(target_pos(1)))^2 + ...
+%                                   (double(Initial_Pos(2)) - double(target_pos(2)))^2);
+                              
 %         waitbar(distance_traveled / distance_total);
 %         drawnow;
     end    
-%     % Close the waitbar
-%     close(wtb);
+
 %     
 %     % If communications to stage was not open to start, then close.
 %     if h_stage_close == 1
