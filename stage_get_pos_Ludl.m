8985e7cb1e7dbceff09a61c583bd4965d29b926f
Jeremy Cribb <cribb@cs.unc.edu>
2020-07-24 12:36:23 -0400

    Changed iterator from i to k.

--- stage_get_pos_Ludl.m
+++ stage_get_pos_Ludl.m
@@ -1,4 +1,4 @@
-function stage=stage_get_pos_Ludl(stage)
+function stage = stage_get_pos_Ludl(stage)
 % STAGE_GET_POS_LUDL returns the position of the stage in Ludl coordinates
 
 
@@ -11,18 +11,18 @@ try
     command_str = sprintf('where x y');
     str_current = stage_send_com_Ludl(stage.handle, command_str);
     
-    i = 1;
-    while numel(str_current) == 0 && i<100
-        pause(.1)
+    k = 1;
+    while numel(str_current) == 0 && (k < 100)
+        pause(0.1);
         str_current = stage_send_com_Ludl(stage.handle, command_str);
-        i = i + 1;
+        k = k + 1;
     end
     
-    if i==100 
-        i/0; %#ok<VUNUS>
+    if k == 100 
+        k/0; %#ok<VUNUS>
     end
     
-    if str_current(2) ~='A'
+    if str_current(2) ~= 'A'
         'A'/0; %#ok<VUNUS>
     end
     
