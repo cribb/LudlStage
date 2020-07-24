d18af3e72f5b41b9d7858167dfe9d9eb01cc0214
Jeremy Cribb <cribb@cs.unc.edu>
2020-07-24 12:38:22 -0400

    Changed the way error codes are processed and commented out the try-catch exception handling because it's obscuring debug output.

--- stage_send_com_Ludl.m
+++ stage_send_com_Ludl.m
@@ -14,7 +14,7 @@ if ~isa(h, 'serial')
     error('First input needs to be a handle to the serial device.');
 end
 
-try
+% try
         
     % Write the command to the stage controller
     for k = 1:size(CommandStr, 2)
@@ -22,7 +22,7 @@ try
     end
     
     % Send the termination character (13) to the stage controller
-    fwrite(h,13);
+    fwrite(h, 13);
     
     % Read the response from the stage controller
     % The response of the stage to 'STATUS' doesn't have the terminator.
@@ -32,35 +32,40 @@ try
         ST_answer = fgetl(h);
     end
     
-    % These are the error messages that can come from the stage
-    if strcmp(':N -1',ST_answer) 
-        stage_log('Unknown command');
-        ST_answer = -1;
-    end
-    if strcmp(':N -2',ST_answer) 
-        stage_log('Illegal point type or axis, or module not installed');
-%         errordlg('Illegal point type or axis, or module not installed!','Stage Error');
-        ST_answer = -1;
-    end
-    if strcmp(':N -3',ST_answer) 
-        stage_log('Not enough parameters (e.g. move r=)');
-%         errordlg('Not enough parameters (e.g. move r=)!','Stage Error');
-        ST_answer = -1;
-    end
-    if strcmp(':N -4',ST_answer) 
-        stage_log('Parameter out of range');
-%         errordlg('Parameter out of range!','Stage Error');
-        ST_answer = -1;
-    end
-    if strcmp(':N -21',ST_answer) 
-        stage_log('Process aborted by HALT command');
-%         errordlg('Process aborted by HALT command!','Stage Error');
-        ST_answer = -1;
+    if contains(ST_answer, ':N')
+        ST_answer = process_error_code(ST_answer);
     end
     
-catch
-    stage_log('Error in Ludl: stage_send_com (S, CommandStr)');
-%         errordlg('Unable to send a command to the stage!','Stage Error');
-    ST_answer = -1;
-end
-end
+% catch
+%     stage_log('Error in Ludl: stage_send_com (S, CommandStr)');
+%     ST_answer = -1;
+% end
+return
+
+
+function ST_answer = process_error_code(ST_answer)
+% These are the error messages that can come from the stage. They come
+% from the stage as a negative number after the string ":N", e.g. ":N -3".
+% This extracts the code and drops the minus sign.
+ErrCode = regexpi(ST_answer, '\:N\s*\-(\d*)', 'tokens');
+ErrCode = (ErrCode{1}{1});
+    
+    switch ErrCode
+        case '1'
+            stage_log('Unknown command');
+            ST_answer = -1;
+        case '2'
+            stage_log('Illegal point type or axis, or module not installed');
+            ST_answer = -1;
+        case '3'
+            stage_log('Not enough parameters (e.g. move r=)');
+            ST_answer = -1;
+        case '4'    
+            stage_log('Parameter out of range');
+            ST_answer = -1;
+        case '21'
+            stage_log('Process aborted by HALT command');
+            ST_answer = -1;
+    end
+    
+    return
